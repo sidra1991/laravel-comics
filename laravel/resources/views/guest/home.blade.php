009b0c670c55208683e590a9b4478b86d9025c03
anthony <diomede1991@libero.it>
2022-04-27 17:41:42 +0200

    header fatto

--- laravel/resources/views/guest/home.blade.php
+++ laravel/resources/views/guest/home.blade.php
@@ -1,1 +1,7 @@
 @extends('guest.porzioni.base')
+
+@section('divSimpatico')
+    <div class="piccolo" >
+        <h3 style="color:white" > CURRENT SERIES </h3>
+    </div>
+@endsection
--- laravel/resources/views/guest/porzioni/base.blade.php
+++ laravel/resources/views/guest/porzioni/base.blade.php
@@ -4,32 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>titolo</title>
 </head>
 <body>
+    <div class="blue_sky">
+
+    </div>
+    <nav>
+        <img src="./img/dc-logo.png" alt="">
+        <ul class="ul_nav" >
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li class="show_li" >SHOPS
+                <ul class="show_ul" >
+                    <li>BO1</li>
+                    <li>BO2</li>
+                    <li>BO3</li>
+                </ul>
+            </li>
+        </ul>
+        <input class="search" type="text" placeholder="Search" >ico
+    </nav>
     <header>
-        <nav>
-            <img src="./img/dc-logo.png" alt="">
-            <ul>
-                <li>CHARACTERS</li>
-                <li>COMICS</li>
-                <li>MOVIES</li>
-                <li>TV</li>
-                <li>GAMES</li>
-                <li>COLLECTIBLES</li>
-                <li>VIDEOS</li>
-                <li>FANS</li>
-                <li>NEWS</li>
-                <li>SHOPS
-                    <ul>
-                        <li>BO1</li>
-                        <li>BO2</li>
-                        <li>BO3</li>
-                    </ul>
-                </li>
-            </ul>
-            <input type="text" placeholder="Search" >ico
-        </nav>
     </header>
+    @yield('divSimpatico')
 </body>
 </html>
