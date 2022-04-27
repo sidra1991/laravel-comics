69f160a675e7c66fe80652225c904bdba153b79f
anthony <diomede1991@libero.it>
2022-04-27 16:21:52 +0200

    alle solite non so che sto facendo

---
+++ laravel/resources/views/guest/home.blade.php
@@ -0,0 +1,1 @@
+@extends('guest.porzioni.base')
---
+++ laravel/resources/views/guest/porzioni/base.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>titolo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="./img/dc-logo.png" alt="">
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOPS
+                    <ul>
+                        <li>BO1</li>
+                        <li>BO2</li>
+                        <li>BO3</li>
+                    </ul>
+                </li>
+            </ul>
+            <input type="text" placeholder="Search" >ico
+        </nav>
+    </header>
+</body>
+</html>
